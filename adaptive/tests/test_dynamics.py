8c2fd79a0c7ddc49139d81a5acd9ba967400606c
rob arbon <robert.arbon@bristol.ac.uk>
2021-03-05 12:57:59 +0000

    dynamics sample accepts a config dict

--- adaptive/tests/test_dynamics.py
+++ adaptive/tests/test_dynamics.py
@@ -20,7 +20,9 @@ def test_dynamics_returns_good_arrays(create_dynamics_engine):
     mm = create_dynamics_engine
     starting_states = np.array([0, 1])
     traj_lengths = np.array([10, 11])
-    trajs = mm.sample(starting_states, traj_lengths)
+    config = {'starting_states': starting_states,
+              'traj_lengths': traj_lengths}
+    trajs = mm.sample(config)
     traj_lengths_correct = np.allclose(traj_lengths, [x.shape[0] for x in trajs])
     start_states_correct = np.allclose(starting_states, [x[0] for x in trajs])
     assert traj_lengths_correct & start_states_correct
@@ -30,24 +32,30 @@ def test_dynamics_bad_start_states(create_dynamics_engine):
     mm = create_dynamics_engine
     starting_states = np.array([3, 1])
     traj_lengths = np.array([10, 10])
+    config = {'starting_states': starting_states,
+              'traj_lengths': traj_lengths}
     with pytest.raises(ValueError):
-        _ = mm.sample(starting_states, traj_lengths)
+        _ = mm.sample(config)
 
 
 def test_dynamics_bad_lengths(create_dynamics_engine):
     mm = create_dynamics_engine
     starting_states = np.array([0, 1])
     traj_lengths = np.array([10, -1])
+    config = {'starting_states': starting_states,
+              'traj_lengths': traj_lengths}
     with pytest.raises(ValueError):
-        _ = mm.sample(starting_states, traj_lengths)
+        _ = mm.sample(config)
 
 
 def test_dynamics_inconsistent_init(create_dynamics_engine):
     mm = create_dynamics_engine
     starting_states = np.array([0, 1, 1])
     traj_lengths = np.array([10, 10])
+    config = {'starting_states': starting_states,
+              'traj_lengths': traj_lengths}
     with pytest.raises(ValueError):
-        _ = mm.sample(starting_states, traj_lengths)
+        _ = mm.sample(config)
 
 
 
