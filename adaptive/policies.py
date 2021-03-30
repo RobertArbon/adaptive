6213ebff109eeb8facf4c92be3c635b85766558b
rob arbon <robert.arbon@bristol.ac.uk>
2021-03-30 10:01:39 +0100

    added and tested naive and inverse microcount policies

---
+++ adaptive/policies.py
@@ -0,0 +1,51 @@
+import numpy as np
+
+from adaptive.containers import CoverageRun
+from adaptive.dynamics import SamplingConfig
+
+
+class SampledSystemStatistics:
+    def __init__(self, cov_run: CoverageRun) -> None:
+        states, counts = np.unique(cov_run.trajectories, return_counts=True)
+        ix = np.argsort(states)
+        self._states = states[ix].astype(int)
+        self._counts = counts[ix].astype(int)
+
+    @property
+    def states(self) -> np.ndarray:
+        return self._states
+
+    @property
+    def counts(self) -> np.ndarray:
+        return self._counts
+
+    @property
+    def inv_count_probability(self) -> np.ndarray:
+        inv_counts = 1.0/self.counts
+        return inv_counts/np.sum(inv_counts)
+
+
+def inverse_microcounts(cov_run: CoverageRun, init_config: SamplingConfig) -> SamplingConfig:
+    if cov_run.n_epochs == 0:
+        return init_config
+    else:
+        stats = SampledSystemStatistics(cov_run)
+        n_walkers = cov_run.epochs[-1].n_walkers
+        traj_length = cov_run.epochs[-1].n_steps
+
+        starting_states = np.random.choice(stats.states, p=stats.inv_count_probability,
+                                           replace=True, size=n_walkers)
+        traj_lengths = np.repeat(traj_length, repeats=n_walkers)
+
+        return SamplingConfig(starting_states=starting_states,
+                              traj_lengths=traj_lengths)
+
+
+def naive_walkers(cov_run: CoverageRun, init_config: SamplingConfig) -> SamplingConfig:
+    if cov_run.n_epochs == 0:
+        return init_config
+    else:
+        starting_states = np.array([walker.trajectory[-1] for walker in cov_run.epochs[-1].walkers])
+        traj_lengths = np.array([walker.trajectory.shape[0] for walker in cov_run.epochs[-1].walkers])
+        return SamplingConfig(starting_states=starting_states,
+                              traj_lengths=traj_lengths)
---
+++ adaptive/tests/test_policies.py
@@ -0,0 +1,71 @@
+import numpy as np
+
+from adaptive.policies import *
+from adaptive.containers import CoverageRun, Epoch, Walker
+
+TRAJ = np.arange(3)
+PROB_TOL = 1e-2
+
+cov_run = CoverageRun()
+cov_run.add(Epoch([Walker(np.array(TRAJ))]))
+cov_run.add(Epoch([Walker(np.array(TRAJ))]))
+
+start_states = np.repeat(0, 2)
+traj_lengths = np.repeat(3, 2)
+init_config = SamplingConfig(starting_states=start_states,
+                             traj_lengths=traj_lengths)
+
+
+def test_sampled_system_stats_states():
+    stats = SampledSystemStatistics(cov_run)
+    assert np.allclose(stats.states, np.array(TRAJ))
+
+
+def test_sampled_system_stats_counts():
+    stats = SampledSystemStatistics(cov_run)
+    assert np.allclose(stats.counts, np.ones_like(TRAJ)*2)
+
+
+def test_sampled_system_stats_inv_prob():
+    stats = SampledSystemStatistics(cov_run)
+    assert np.allclose(stats.inv_count_probability, np.ones_like(TRAJ)/TRAJ.shape[0])
+
+
+def test_inverse_microcounts_init():
+    cov_run_empty = CoverageRun()
+    new_config = inverse_microcounts(cov_run_empty, init_config=init_config)
+
+    states_ok = np.allclose(new_config.starting_states, init_config.starting_states)
+    lengths_ok = np.allclose(new_config.traj_lengths, init_config.traj_lengths)
+    assert states_ok and lengths_ok
+
+
+def test_inverse_microcounts_with_data_probs():
+    new_configs = [inverse_microcounts(cov_run, init_config=init_config)
+                   for _ in range(10000)]
+    all_states = np.concatenate([x.starting_states for x in new_configs])
+    states, counts = np.unique(all_states, return_counts=True)
+    state_probs = counts/np.sum(counts)
+    assert np.mean(np.abs(state_probs-np.ones_like(TRAJ)/TRAJ.shape[0])) < PROB_TOL
+
+
+def test_inverse_microcounts_with_data_lengths():
+    new_config = inverse_microcounts(cov_run, init_config=init_config)
+    lengths = new_config.traj_lengths
+    assert np.allclose(lengths, traj_lengths)
+
+
+def test_naive_walkers_init():
+    cov_run_empty = CoverageRun()
+    new_config = naive_walkers(cov_run_empty, init_config=init_config)
+    states_ok = np.allclose(new_config.starting_states, init_config.starting_states)
+    lengths_ok = np.allclose(new_config.traj_lengths, init_config.traj_lengths)
+    assert states_ok and lengths_ok
+
+
+def test_naive_walkers():
+    new_config = naive_walkers(cov_run, init_config=init_config)
+    states_ok = np.allclose(new_config.starting_states, np.repeat(TRAJ[-1], cov_run.epochs[-1].n_walkers))
+    lengths_ok = np.allclose(new_config.traj_lengths, np.repeat(TRAJ.shape[0], cov_run.epochs[-1].n_walkers))
+    assert states_ok and lengths_ok
+
